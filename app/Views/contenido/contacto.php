8241c6971c0620f53763317ddd1f50ef1bbf6957
Tomás Ariel Conti <taconti02@gmail.com>
2025-04-18 00:29:03 -0300

    Terminos y contacto
    Ahi corregi terminos de uso y contacto. Le agregue estilo.

--- app/Views/contenido/contacto.php
+++ app/Views/contenido/contacto.php
@@ -1,5 +1,5 @@
 
-<section>
+<section id="contacto">
     
 <h1>Información de Contacto</h1>
     
@@ -15,6 +15,7 @@
 
 <div>
 <br>
+<h2><center>Ubicación</center></h2>
 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3540.0900624775923!2d-58.834789226832285!3d-27.46645537632145!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94456ca6d24ec0c9%3A0xb92ce3fedb0d7729!2sFacultad%20de%20Ciencias%20Exactas%20y%20Naturales%20y%20Agrimensura!5e0!3m2!1ses!2sar!4v1744844605126!5m2!1ses!2sar" width=100% height="450" style="border:0;" allowfullscreen="" loading="lazy" ></iframe>
 </div>
 <div>
@@ -31,7 +32,7 @@
   <div class="col-md-4">
     <label for="validationDefaultUsername" class="form-label">Correo electrónico</label>
     <div class="input-group">
-      <span class="input-group-text" id="inputGroupPrepend2">@</span>
+    
       <input type="text" class="form-control" id="validationDefaultUsername" aria-describedby="inputGroupPrepend2" required>
     </div>
   </div>
--- app/Views/contenido/terminos_y_condiciones.php
+++ app/Views/contenido/terminos_y_condiciones.php
@@ -1,32 +1,40 @@
-
-<head>
-<title>Términos y Condiciones</title>
-</head>
 <body>
-    <h1>Términos y Usos</h1>
-    
-    <h2>1. Uso del Sitio Web</h2>
-    <p>Al acceder a FC Box, aceptas los siguientes términos:</p>
-    <ul>
-        <li>Solo vendemos videojuegos digitales; no ofrecemos productos físicos.</li>
-        <li>FC Box se reserva el derecho de modificar precios y condiciones sin previo aviso.</li>
-    </ul>
+    <div id="terminos">
+        <h1>Términos y Condiciones</h1>
 
-    <h2>2. Servicios Ofrecidos</h2>
-    <p>FC Box comercializa videojuegos digitales de distintas plataformas.</p>
+        <h2>1. Uso del Sitio Web</h2>
+        <div class="legal">
+            <p>Al acceder a FC Box, aceptas los siguientes términos:</p>
+            <ul>
+                <li>Solo vendemos videojuegos digitales; no ofrecemos productos físicos.</li>
+                <li>FC Box se reserva el derecho de modificar precios y condiciones sin previo aviso.</li>
+            </ul>
+        </div><br>
 
-    <h2>3. Políticas de Privacidad</h2>
-    <p>Protegemos tu información personal y solo la utilizamos para procesar pedidos y mejorar la experiencia del usuario.</p>
+        <h2>2. Servicios Ofrecidos</h2>
+        <div class="legal">
+        <li>FC Box comercializa videojuegos digitales de distintas plataformas.</li>
+        </div><br>
 
-    <h2>4. Procedimientos de Venta</h2>
-    <ul>
-        <li>Una vez confirmado el pago, recibirás un código de activación por correo electrónico.</li>
-        <li>El acceso al juego dependerá de la plataforma de cada título (Steam, PlayStation, Xbox, etc.).</li>
-    </ul>
+        <h2>3. Políticas de Privacidad</h2>
+        <div class="legal">
+            <li>Protegemos tu información personal y solo la utilizamos para procesar pedidos y mejorar la experiencia del usuario.</li>
+        </div><br>
 
-    <h2>5. Garantías y Soporte Postventa</h2>
-    <ul>
-        <li>Los códigos digitales son entregados de inmediato y no pueden ser reembolsados una vez usados.</li>
-        <li>Para cualquier problema con tu código de activación, contáctanos en <a href="mailto:soporte@fcbox.com.ar">soporte@fcbox.com.ar</a>.</li>
-    </ul>
+        <h2>4. Procedimientos de Venta</h2>
+        <div class="legal">
+        <ul>
+            <li>Una vez confirmado el pago, recibirás un código de activación por correo electrónico.</li>
+            <li>El acceso al juego dependerá de la plataforma de cada título (Steam, PlayStation, Xbox, etc.).</li>
+        </ul>
+        </div><br>
+
+        <h2>5. Garantías y Soporte Postventa</h2>
+        <div class="legal">
+            <ul>
+                <li>Los códigos digitales son entregados de inmediato y no pueden ser reembolsados una vez usados.</li>
+                <li>Para cualquier problema con tu código de activación, contáctanos en <a href="mailto:soporte@fcbox.com.ar">soporte@fcbox.com.ar</a>.</li>
+            </ul>
+        </div>
+    </div>
 </body>
