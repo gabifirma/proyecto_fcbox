bfac8673ba0264c695fc1250373fe7b1986ce79f
gabifirma <gabfirmapaz@gmail.com>
2025-05-22 10:14:35 -0300

    cambios en principal y galeria 22/5/25

--- app/Views/contenido/cont_galeria.php
+++ app/Views/contenido/cont_galeria.php
@@ -1,6 +1,6 @@
   <section class="mt-4 container">
     
-    <div class="row row-cols-1 row-cols-md-3 g-4" id="card1">
+    <div class="row row-cols-1 row-cols-md-3 g-4">
       <div class="col">
         <div class="card">
           <img src="assets/img/juego13.jpg" class="img-fluid card-img-top" alt="...">
@@ -15,74 +15,89 @@
       <div class="col">
         <div class="card">
           <img src="assets/img/juego10.jpg" class="img-fluid card-img-top" alt="...">
-            <a href="<?php echo base_url('galeria'); ?>" class="stretched-link"></a>
+          <div class="capa">
+            <h4>hola gabi</h4>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <a href="<?php echo base_url('galeria'); ?>" class="stretched-link"></a>
         </div>
       </div>
     
       <div class="col">
         <div class="card">
           <img src="assets/img/juego6.jpg" class="img-fluid card-img-top" alt="...">
-            <a href="<?php echo base_url('galeria'); ?>" class="stretched-link"></a>            
+          <div class="capa">
+            <h4>hola gabi</h4>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <a href="<?php echo base_url('galeria'); ?>" class="stretched-link"></a>            
         </div>
       </div>
     
       <div class="col">
         <div class="card">
           <img src="assets/img/juego7.jpg" class="img-fluid card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">FIFA 24</h5>
-            <a href="<?php echo base_url('galeria'); ?>" class="stretched-link"></a>
+          <div class="capa">
+            <h4>hola gabi</h4>
+            <p>Lorem ipsum dolor sit amet.</p>
           </div>
+          <a href="<?php echo base_url('galeria'); ?>" class="stretched-link"></a>
         </div>
       </div>
+      
     
       <div class="col">
         <div class="card">
           <img src="assets/img/juego8.jpg" class="img-fluid card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Halo Infinite</h5>
-            <a href="<?php echo base_url('galeria'); ?>" class="stretched-link"></a>
+          <div class="capa">
+            <h4>hola gabi</h4>
+            <p>Lorem ipsum dolor sit amet.</p>
           </div>
+          <a href="<?php echo base_url('galeria'); ?>" class="stretched-link"></a>
         </div>
       </div>
     
       <div class="col">
         <div class="card">
           <img src="assets/img/juego9.jpg" class="img-fluid card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Call Of Duty: Black Ops 6</h5>
-            <a href="<?php echo base_url('galeria'); ?>" class="stretched-link"></a>
+          <div class="capa">
+            <h4>hola gabi</h4>
+            <p>Lorem ipsum dolor sit amet.</p>
           </div>
+          <a href="<?php echo base_url('galeria'); ?>" class="stretched-link"></a>
         </div>
       </div>
     
       <div class="col">
         <div class="card">
           <img src="assets/img/juego11.jpg" class="img-fluid card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Forza Horizon 5</h5>
-            <a href="<?php echo base_url('galeria'); ?>" class="stretched-link"></a>
+          <div class="capa">
+            <h4>hola gabi</h4>
+            <p>Lorem ipsum dolor sit amet.</p>
           </div>
+          <a href="<?php echo base_url('galeria'); ?>" class="stretched-link"></a>
         </div>
       </div>
     
       <div class="col">
         <div class="card">
           <img src="assets/img/juego12.jpg" class="img-fluid card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">State Of Decay 2: Juggernaut Edition</h5>
-            <a href="<?php echo base_url('galeria'); ?>" class="stretched-link"></a>
+          <div class="capa">
+            <h4>hola gabi</h4>
+            <p>Lorem ipsum dolor sit amet.</p>
           </div>
+          <a href="<?php echo base_url('galeria'); ?>" class="stretched-link"></a>
         </div>
       </div>
     
       <div class="col">
         <div class="card">
           <img src="assets/img/juego14.jpg" class="img-fluid card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Among Us</h5>
-            <a href="<?php echo base_url('galeria'); ?>" class="stretched-link"></a>
+          <div class="capa">
+            <h4>hola gabi</h4>
+            <p>Lorem ipsum dolor sit amet.</p>
           </div>
+          <a href="<?php echo base_url('galeria'); ?>" class="stretched-link"></a>
         </div>
       </div>
 
--- app/Views/contenido/cont_principal.php
+++ app/Views/contenido/cont_principal.php
@@ -31,109 +31,42 @@
     <a class="btn btn-outline-success" href="<?php echo base_url('galeria'); ?>" role="button">Mostrar todo</a>
   </div>
   
-  <div class="row row-cols-1 row-cols-md-3 g-1 " id="card2">
-    <div class="card mb-5 mx-auto" style="max-width: 340px;">
-      <div class="row g-0">
-        <div class="col">
-          <img src="assets/img/juego8.jpg" class="img-fluid rounded-start" alt="...">
+  <div class="row row-cols-1 row-cols-md-3 g-1">
+    <div class="col">
+      <div class="card">
+        <img src="assets/img/juego8.jpg" class="img-fluid card-img-top" alt="...">
+        <div class="capa">
+          <h4>hola gabi</h4>
+          <p>Lorem ipsum dolor sit amet.</p>
         </div>
         <a href="<?php echo base_url('galeria'); ?>" class="stretched-link"></a>
-      </div>
-      <!-- Button trigger modal -->
-      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalUno">
-        Leer más
-      </button>
-      <!-- Modal -->
-      <div class="modal fade" id="modalUno" tabindex="-1">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-            <h5 class="modal-title">Halo Infinite</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-              <strong>Descripción:</strong> Cuando se pierde toda esperanza y el destino de la humanidad pende de un hilo, 
-              el Jefe Maestro está listo para enfrentarse al enemigo más despiadado al que se haya enfrentado. Entra en la 
-              armadura del héroe más grande de la humanidad para experimentar una aventura épica de mundo abierto y explora 
-              las enormes dimensiones del anillo Halo. <br> <br>
-              <strong>Desarrollador:</strong> 343 Industries. <br> <br>
-              <strong>Precio:</strong> ARS$ 79.999,00.
-            </div>
-          </div>
-        </div>
       </div>
     </div>
 
-    <div class="card mb-5 mx-auto" style="max-width: 340px;">
-      <div class="row g-0">
-        <div class="col">
-          <img src="assets/img/juego14.jpg" class="img-fluid rounded-start" alt="...">
+    <div class="col">
+      <div class="card">
+        <img src="assets/img/juego14.jpg" class="img-fluid card-img-top" alt="...">
+        <div class="capa">
+          <h4>hola gabi</h4>
+          <p>Lorem ipsum dolor sit amet.</p>
         </div>
         <a href="<?php echo base_url('galeria'); ?>" class="stretched-link"></a>
-      </div>
-      <!-- Button trigger modal -->
-      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalDos">
-        Leer más
-      </button>
-      <!-- Modal -->
-      <div class="modal fade" id="modalDos" tabindex="-1">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-            <h5 class="modal-title">Among Us</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-              <strong>Descripción:</strong> Un juego exclusivamente online donde la verdadera oposición son los otros 
-              jugadores que están contigo. Cada juego será diferente dependiendo del rol que estés jugando y 
-              de la experiencia o astucia de los otros jugadores. <br> <br>
-              <strong>Desarrollador:</strong> Innersloth. <br> <br>
-              <strong>Precio:</strong> ARS$ 79,00.
-            </div>
-          </div>
-        </div>
       </div>
     </div>
 
-    <div class="card mb-5 mx-auto" style="max-width: 340px;">
-      <div class="row g-0">
-        <div class="col">
-          <img src="assets/img/juego7.jpg" class="img-fluid rounded-start" alt="...">
+    <div class="col">
+      <div class="card">
+        <img src="assets/img/juego7.jpg" class="img-fluid card-img-top" alt="...">
+        <div class="capa">
+          <h4>hola gabi</h4>
+          <p>Lorem ipsum dolor sit amet.</p>
         </div>
         <a href="<?php echo base_url('galeria'); ?>" class="stretched-link"></a>
       </div>
-      <!-- Button trigger modal -->
-      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalTres">
-        Leer más
-      </button>
-      <!-- Modal -->
-      <div class="modal fade" id="modalTres" tabindex="-1">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-            <h5 class="modal-title">FIFA 24</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-              <strong>Descripción:</strong> Este  es el comienzo del futuro del fútbol. Con este título innovador y 
-              auténtico, te sentirás mucho más cerca del juego gracias a una experiencia futbolística más real 
-              que nunca y a la compañía de los mejores jugadores de los equipos, ligas y competencias más grandes
-              del mundo.
-              <br>
-              <br>
-              <strong>Desarrollador:</strong> EA Canadá.
-              <br>
-              <br>
-              <strong>Precio:</strong> ARS$ 48.999,00.
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
+
   </div>
 
-  
-  
 </section>
 
 <section class="mt-5 container">
@@ -142,103 +75,43 @@
     <h1>Ofertas Mensuales</h1>
   </div>
 
-  <div class="row row-cols-1 row-cols-md-3 g-1 " id="card2">
-    <div class="card mb-5 mx-auto" style="max-width: 340px;">
-      <div class="row g-0">
-        <div class="col">
-          <img src="assets/img/juego7.jpg" class="img-fluid rounded-start" alt="...">
+  <div class="row row-cols-1 row-cols-md-3 g-1">
+
+    <div class="col">
+      <div class="card">
+        <img src="assets/img/juego7.jpg" class="img-fluid card-img-top" alt="...">
+        <div class="capa">
+          <h4>hola gabi</h4>
+          <p>Lorem ipsum dolor sit amet.</p>
         </div>
         <a href="<?php echo base_url('galeria'); ?>" class="stretched-link"></a>
       </div>
-      <!-- Button trigger modal -->
-      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalCuatro">
-        Leer más
-      </button>
-      <!-- Modal -->
-      <div class="modal fade" id="modalCuatro" tabindex="-1">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-            <h5 class="modal-title">FIFA 24</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-              <strong>Descripción:</strong> Este  es el comienzo del futuro del fútbol. Con este título innovador y 
-              auténtico, te sentirás mucho más cerca del juego gracias a una experiencia futbolística más real 
-              que nunca y a la compañía de los mejores jugadores de los equipos, ligas y competencias más grandes
-              del mundo.
-              <br>
-              <br>
-              <strong>Desarrollador:</strong> EA Canadá.
-              <br>
-              <br>
-              <strong>Precio:</strong> ARS$ 48.999,00.
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
-    <div class="card mb-5 mx-auto" style="max-width: 340px;">
-      <div class="row g-0">
-        <div class="col">
-          <img src="assets/img/juego15.jpg" class="img-fluid rounded-start" alt="...">
+
+    <div class="col">
+      <div class="card">
+        <img src="assets/img/juego15.jpg" class="img-fluid card-img-top" alt="...">
+        <div class="capa">
+          <h4>hola gabi</h4>
+          <p>Lorem ipsum dolor sit amet.</p>
         </div>
         <a href="<?php echo base_url('galeria'); ?>" class="stretched-link"></a>
       </div>
-      <!-- Button trigger modal -->
-      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalCinco">
-        Leer más
-      </button>
-      <!-- Modal -->
-      <div class="modal fade" id="modalCinco" tabindex="-1">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-            <h5 class="modal-title">Battlefield 4</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-              <strong>Descripción:</strong> Un juego de acción ambientado de nuevo en la guerra moderna, con una espectacular y 
-              cinematográfica campaña para un jugador y sobre todo con un ambicioso multijugador competitivo, con 
-              batallas hasta para 64 jugadores, vehículos, escenarios destructibles y clima dinámico. <br><br>
-              <strong>Desarrollador:</strong> EA Digital Illusions CE.<br><br>
-              <strong>Precio:</strong> ARS$ 8.999,00.
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
-    <div class="card mb-5 mx-auto" style="max-width: 340px;">
-      <div class="row g-0">
-        <div class="col">
-          <img src="assets/img/juego17.jpg" class="img-fluid rounded-start" alt="...">
+
+    <div class="col">
+      <div class="card">
+        <img src="assets/img/juego17.jpg" class="img-fluid card-img-top" alt="...">
+        <div class="capa">
+          <h4>hola gabi</h4>
+          <p>Lorem ipsum dolor sit amet.</p>
         </div>
         <a href="<?php echo base_url('galeria'); ?>" class="stretched-link"></a>
       </div>
-      <!-- Button trigger modal -->
-      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalSeis">
-        Leer más
-      </button>
-      <!-- Modal -->
-      <div class="modal fade" id="modalSeis" tabindex="-1">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-            <h5 class="modal-title">Doom</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-             <strong>Descripción:</strong> La serie se centra en las hazañas de un marine espacial no identificado que opera bajo 
-              las órdenes de la Union Aerospace Corporation (UAC), que lucha contra hordas de demonios y no muertos en 
-              el inframundo y la Tierra, protagonizando diversos viajes por portales entre ambos mundos. <br><br>
-              <strong>Desarrollador:</strong> id Software.<br><br>
-              <strong>Precio:</strong> ARS$ 8.799,00.
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
+
   </div>
+
 </section>
 
 <section class="mt-5 container">
@@ -247,100 +120,42 @@
     <h1>Novedades</h1>
   </div>
 
-  <div class="row row-cols-1 row-cols-md-3 g-1" id="card2">
-    <div class="card mb-5 mx-auto" style="max-width: 350px;">
-      <div class="row g-0">
-        <div class="col">
-          <img src="assets/img/nuevo1.jpg" class="img-fluid rounded-start" alt="...">
+  <div class="row row-cols-1 row-cols-md-3 g-1">
+    <div class="col">
+      <div class="card">
+        <img src="assets/img/nuevo1.jpg" class="img-fluid card-img-top" alt="...">
+        <div class="capa">
+          <h4>hola gabi</h4>
+          <p>Lorem ipsum dolor sit amet.</p>
         </div>
         <a href="<?php echo base_url('galeria'); ?>" class="stretched-link"></a>
-      </div>
-      <!-- Button trigger modal -->
-      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalSiete">
-        Leer más
-      </button>
-      <!-- Modal -->
-      <div class="modal fade" id="modalSiete" tabindex="-1">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-            <h5 class="modal-title">Batman Knigths</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-              <strong>Descripción:</strong> Es un RPG de mundo abierto, ambientado en la versión más dinámica e interactiva de 
-              Ciudad Gótica hasta ahora. Juega en solitario o con otro héroe y patrulla los cinco distritos de 
-              Ciudad Gótica para sorprender a los criminales dondequiera que se encuentren.<br><br>
-              <strong>Desarrollador:</strong> Warner Bros. Games Montreal.<br><br>
-              <strong>Precio:</strong> ARS$ 7.999,00.
-            </div>
-          </div>
-        </div>
       </div>
     </div>
 
-    <div class="card mb-5 mx-auto" style="max-width: 350px;">
-      <div class="row g-0">
-        <div class="col">
-          <img src="assets/img/nuevo2.jpg" class="img-fluid rounded-start" alt="...">
+    <div class="col">
+      <div class="card">
+        <img src="assets/img/nuevo2.jpg" class="img-fluid card-img-top" alt="...">
+        <div class="capa">
+          <h4>hola gabi</h4>
+          <p>Lorem ipsum dolor sit amet.</p>
         </div>
         <a href="<?php echo base_url('galeria'); ?>" class="stretched-link"></a>
       </div>
-      <!-- Button trigger modal -->
-      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalOcho">
-        Leer más
-      </button>
-      <!-- Modal -->
-      <div class="modal fade" id="modalOcho" tabindex="-1">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-            <h5 class="modal-title">Age Of Empires IV: Anniversary Edition</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-              <strong>Descripción:</strong> Combina lo que ya le funcionó en su momento con fórmulas innovadoras para que 
-              disfrutes expandiendo tu imperio hasta donde alcancen tus ojos, todo ello con una impresionante 
-              fidelidad visual en 4K que eleva a este juego de estrategia en tiempo real a otro nivel.<br><br>
-              <strong>Desarrollador:</strong> Relic Enterteiment.<br><br>
-              <strong>Precio:</strong> ARS$ 17.599,00.
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
-
-    <div class="card mb-5 mx-auto" style="max-width: 350px;">
-      <div class="row g-0">
-        <div class="col">
-          <img src="assets/img/nuevo3.jpg" class="img-fluid rounded-start" alt="...">
+    
+    <div class="col">
+      <div class="card">
+        <img src="assets/img/nuevo3.jpg" class="img-fluid card-img-top" alt="...">
+        <div class="capa">
+          <h4>hola gabi</h4>
+          <p>Lorem ipsum dolor sit amet.</p>
         </div>
         <a href="<?php echo base_url('galeria'); ?>" class="stretched-link"></a>
       </div>
-      <!-- Button trigger modal -->
-      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalNueve">
-        Leer más
-      </button>
-      <!-- Modal -->
-      <div class="modal fade" id="modalNueve" tabindex="-1">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-            <h5 class="modal-title">Mortal Kombat 11</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-              <strong>Descripción:</strong> Se trata de la undécima secuela de una serie de títulos de combate y peleas 
-              de marcada estructura 2D, que en esta ocasión, además de añadir los mejores gráficos de la serie, 
-              presentará luchadores clásicos y nuevos, más modos de juego y una oferta online que quiere marcar un antes y un después.<br><br>
-              <strong>Desarrollador:</strong> NetherRealm Studios.<br><br>
-              <strong>Precio:</strong> ARS$ 28.999,00.
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
+    
   </div>
+
 </section>
 
 <section class="mt-3 container-xl">
